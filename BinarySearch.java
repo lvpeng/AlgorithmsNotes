b69cd8fd22a8f052e8cd33a798bc6fe96c71a872
EvanLyu <lvpeng-333@163.com>
2017-07-17 15:55:32 +0800

    update

---
+++ BinarySearch.java
@@ -0,0 +1,18 @@
+//import java.util.Arrays;
+//
+//public class BinarySearch{
+//  public static int rank(int key, int[] a){
+//    int lo = 0;
+//    int hi = a.length -1;
+//        
+//    mid = lo + (hi -lo) /2;
+//    
+//    if (key < a[mid]) {
+//      hi = mid ;
+//    } else {
+//      lo = mid;
+//    }
+//    
+//    
+//  }
+//}
